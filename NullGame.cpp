4daada2d3c19895df354507cab24ffa9f4f38f24
Alex Vorontsov <nameless323@gmail.com>
2017-10-31 21:47:29 +0300

    Init engine callback.

--- NullGame.cpp
+++ NullGame.cpp
@@ -6,11 +6,19 @@
 #include "stdafx.h"
 
 #include <Core/KiotoEngine.h>
+#include <Core/Scene.h>
 
 #include <windows.h>
 
+void OnEngineInited()
+{
+    OutputDebugStringA("init engine");
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int nCmdShow)
 {
-    Kioto::KiotoMain(hInstance, prevInstance, cmdLine, nCmdShow, L"Kioto game");
+    Kioto::KiotoMain(hInstance, prevInstance, cmdLine, nCmdShow, L"Kioto game", OnEngineInited);
+    //Kioto::Scene* scene = new Kioto::Scene();
+    //Kioto::SetScene(scene);
     return 0;
 }
