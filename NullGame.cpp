39764e85cf2cb86ab88f889cdbd666ad4204fb78
Alex Vorontsov <nameless323@gmail.com>
2017-10-31 22:43:57 +0300

    Init engine callback, include kioto header, pass scene.

--- NullGame.cpp
+++ NullGame.cpp
@@ -5,20 +5,19 @@
 
 #include "stdafx.h"
 
-#include <Core/KiotoEngine.h>
-#include <Core/Scene.h>
+#include <Kioto.h>
 
 #include <windows.h>
 
 void OnEngineInited()
 {
+    Kioto::Scene* scene = new Kioto::Scene();
+    Kioto::SetScene(scene);
     OutputDebugStringA("init engine");
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int nCmdShow)
 {
     Kioto::KiotoMain(hInstance, prevInstance, cmdLine, nCmdShow, L"Kioto game", OnEngineInited);
-    //Kioto::Scene* scene = new Kioto::Scene();
-    //Kioto::SetScene(scene);
     return 0;
 }
