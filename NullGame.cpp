7d37b489b97647d3b2701c243ec0e5252bf7f767
Alex Vorontsov <nameless323@gmail.com>
2020-05-10 12:25:42 -0400

    Camera controls

--- NullGame.cpp
+++ NullGame.cpp
@@ -8,6 +8,7 @@
 #include <Kioto.h>
 
 #include <sstream>
+#include <iostream>
 
 class TestScene : public Kioto::Scene
 {
@@ -60,50 +61,23 @@ public:
             thisFrameOffset.y = mouseRelativePosition.y * mouseAccel;
         }
 
-        Matrix4 localRot = Matrix4::Identity;
-        if (scroll == eMouseWheelScroll::ScrollNone && Input::GetMouseHeldDown(Kioto::eMouseCodes::MouseLeft))
+        Matrix4 worldRotation = Matrix4::Identity;
+
+        static float32 xAccum = 0; 
+        static float32 yAccum = 0;
+        if (scroll == eMouseWheelScroll::ScrollNone && Input::GetMouseHeldDown(Kioto::eMouseCodes::MouseRight))
         {
-            Matrix4 xRot = Matrix4::BuildRotationX(Kioto::Math::DegToRad(mouseRelativePosition.y * mouseRotationAccel));
-            Matrix4 yRot = Matrix4::BuildRotationY(Kioto::Math::DegToRad(mouseRelativePosition.x * mouseRotationAccel));
-            localRot *= yRot * xRot;
+            xAccum += mouseRelativePosition.y * mouseRotationAccel;
+            yAccum += mouseRelativePosition.x * mouseRotationAccel;
         }
-        Matrix4 worldRotation = localRot * cameraTransform->GetToWorld();
+        Matrix4 xRot = Matrix4::BuildRotationX(Kioto::Math::DegToRad(xAccum));
+        Matrix4 yRot = Matrix4::BuildRotationY(Kioto::Math::DegToRad(yAccum));
+        worldRotation = yRot * xRot;
 
         Vector3 worldPos = cameraTransform->TransformPointToWorld(thisFrameOffset);
+
         cameraTransform->SetWorldPosition(worldPos);
-
         cameraTransform->SetWorldRotation(worldRotation);
-        //auto mouse = Kioto::Input::GetMouseRelativePosition(); //<-- check all buttons
-        //    ss << __FILE__ << "(" << __LINE__ << ")" << " x: " << mouse.x << " | y: " << mouse.y << std::endl;
-        //if (Kioto::Input::GetIsButtonHeldDown(Kioto::eKeyCode::KeyCodeA))
-        //    left -= 5.0f * dt;
-        //else if (Kioto::Input::GetIsButtonHeldDown(Kioto::eKeyCode::KeyCodeD))
-        //    left += 5.0f * dt;
-        //else if (Kioto::Input::GetIsButtonHeldDown(Kioto::eKeyCode::KeyCodeW))
-        //    fwd += 5.0f * dt;
-        //else if (Kioto::Input::GetIsButtonHeldDown(Kioto::eKeyCode::KeyCodeS))
-        //    fwd -= 5.0f * dt;
-
-        // Kioto::Matrix4 rot = Kioto::Matrix4::Identity;
-        // if (Kioto::Input::GetIsButtonHeldDown(Kioto::eKeyCode::KeyCodeJ))
-        //     rotY -= 25.0f * dt;
-        // else if (Kioto::Input::GetIsButtonHeldDown(Kioto::eKeyCode::KeyCodeL))
-        //     rotY += 25.0f * dt;
-        // else if (Kioto::Input::GetIsButtonHeldDown(Kioto::eKeyCode::KeyCodeI))
-        //     rotX += 25.0f * dt;
-        // else if (Kioto::Input::GetIsButtonHeldDown(Kioto::eKeyCode::KeyCodeK))
-        //     rotX -= 25.0f * dt;
-        // rot *= Kioto::Matrix4::BuildRotationX(Kioto::Math::DegToRad(rotX)) * Kioto::Matrix4::BuildRotationY(Kioto::Math::DegToRad(rotY));
-        // teapotTransform->SetWorldRotation(rot);
-
-        /*if (Kioto::Input::GetButtonUp(Kioto::eKeyCode::KeyCodeSpace))
-        {
-            fwd = 1.0f;
-            left = 0.0f;
-
-            rotX = 0.0f;
-            rotY = 0.0f;
-        }*/
     }
     ~TestSceneSystem()
     {
@@ -118,35 +92,6 @@ public:
 
 void OnEngineInit()
 {
-    /*Kioto::Scene* scene = new TestScene("Main scene");
-    Kioto::SetScene(scene);
-
-    Kioto::Entity* cameraEntity = new Kioto::Entity();
-    cameraEntity->SetName("Camera");
-    cameraEntity->AddComponent(cameraTransform);
-
-    cam = new Kioto::CameraComponent(Kioto::Math::DegToRad(60.0f), Kioto::Renderer::GetAspect(), 0.01f, 100.0f);
-    cam->SetIsMain(true);
-    cameraEntity->AddComponent(cam);
-    cameraTransform->SetWorldRotation(Kioto::Matrix4::Identity);
-    cameraTransform->SetWorldPosition({ 0.0f, 0.0f, -5.0f });
-
-    scene->AddSystem(new TestSceneSystem{});
-    scene->AddEntity(cameraEntity);
-
-    Kioto::Entity* teapotEntity = new Kioto::Entity();
-    teapotEntity->SetName("Teapot");
-    teapotTransform->SetWorldPosition({ 1.0f, 0.0f, 2.0f });
-    teapotEntity->AddComponent(teapotTransform);
-
-    Kioto::RenderComponent* teapotRenderer = new Kioto::RenderComponent();
-    teapotRenderer->SetMaterial("Materials\\Test.mt");
-    teapotRenderer->SetMesh("Models\\Teapot.fbx");
-    teapotEntity->AddComponent(teapotRenderer);
-
-    scene->AddEntity(teapotEntity);
-
-    //Kioto::SaveScene("C:\\Repos\\NullGame\\scenes\\scene.ksc");*/
     Kioto::LoadScene("C:\\Repos\\NullGame\\scenes\\scene.ksc");
     Kioto::Scene* scene = Kioto::GetScene();
     scene->AddSystem(new TestSceneSystem{});
